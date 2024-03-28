505ef01e39f65bae9b1c3507f2936811457add0d
Aleksandar Buljevic <abuljevic8@gmail.com>
2024-03-28 09:47:18 +0100

    Add check have hour passed

--- src/app/services/request-saver.service.ts
+++ src/app/services/request-saver.service.ts
@@ -8,19 +8,28 @@ export class RequestSaverService {
 
   saveData(name: string, data: any) {
     localStorage.setItem(name, JSON.stringify(data));
+    localStorage.setItem(name + '-time', new Date().toString());
   }
 
   getAndDeleteData(name: string): any {
     let campData = localStorage.getItem(name);
+    let time = localStorage.getItem(name + '-time');
 
-    // TODO: Ako je proslo sat vremena od proslog puta; obrisi i vrati null
-    if (false) {
-      localStorage.removeItem(name);
-    }
-
-    if (!campData) {
+    if (!campData || !time) {
       return null;
     }
+
+    let differenceMs = Math.abs(
+      new Date().getTime() - new Date(time).getTime()
+    );
+    let differenceMinutes = differenceMs / (1000 * 60);
+
+    if (differenceMinutes > 60) {
+      localStorage.removeItem(name);
+      localStorage.removeItem(name + '-time');
+      return null;
+    }
+
     return JSON.parse(campData);
   }
 }
